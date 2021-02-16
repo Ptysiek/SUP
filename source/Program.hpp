d7f5a249254f8cc1f30fbadc29e127491202d64a
Kacu <k.ptychu@gmail.com>
2021-02-16 15:54:24 +0100

    project cleanup

--- source/Program.hpp
+++ source/Program.hpp
@@ -1,15 +1,11 @@
 #pragma once
 
-
 #include "File.hpp"
 #include "FileBuilder.hpp"
-#include "FileIO.hpp"
-#include "PathInitializer.hpp"
-#include "ProjectTree.hpp"
+#include "ProjectTreeBuilder.hpp"
 
 
 class Program {
-
     const std::string targetPath_;
     ProjectTree data_;
 
@@ -20,23 +16,10 @@ public:
     {}
 
     void execute() {
-        auto paths = FileIO::readPaths(targetPath_);
-        
-        ProcessPaths(paths);
-
+        ProjectTreeBuilder builder(targetPath_);
+        data_ = builder.getProduct();
     }
 
 private:
-    void ProcessPaths(const std::vector<std::string>& paths) {
-        for (const auto& path : paths) {
-            PathInitializer initializer(path);
-            FileBuilder builder(initializer);
-            data_.push(builder.getFile());
-        }
-    }
-    std::vector<std::string> ReadSubdirectories(const std::string& path) {
-
-    }
-
 };
 
--- source/ProjectTreeBuilder.hpp
+++ source/ProjectTreeBuilder.hpp
@@ -1,19 +1,50 @@
 #pragma once
 
-#include "ProjectTree.hpp"
+#include <string>
 
+#include "FileIO.hpp"
+#include "PathInitializer.hpp"
+
+
+using ProjectTree = std::vector<File>;
 
 class ProjectTreeBuilder {
-
+    const std::string startPath_;
     ProjectTree product_;
 
 public:
-    ProjectTreeBuilder():
+    ProjectTreeBuilder(const std::string& startPath):
+        startPath_(startPath),
         product_()
-    {}
+    {
+        BuildProduct();
+    }
 
     ProjectTree getProduct() const { return product_; }
     
+private:
+    void BuildProduct() {
+        auto subfiles = ReadRecursive(startPath_);
+    }
+
+    std::vector<File> ReadRecursive(const std::string& targetPath) {
+        auto paths = FileIO::readPaths(targetPath);
+        std::vector<File> files;
+
+        for (const auto& path : paths) {
+            auto subfiles = ReadRecursive(path);
+            auto file = ProcessData(path, subfiles); 
+            files.push_back(file);
+        }
+        return files;
+    }
+
+    File ProcessData(const std::string& path, std::vector<File>& subFiles) {
+        PathInitializer initializer(path);
+        FileBuilder builder(initializer, subFiles);
+        //data_.push(builder.getProduct());
+        return builder.getProduct();
+    }
 
 
 
