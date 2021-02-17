458f13fe8ab82a533b8f66589476003d53656c5c
Kacu <k.ptychu@gmail.com>
2021-02-17 13:48:11 +0100

    new ut: PathInitializer: static operations

--- source/PathInitializer.ut.cpp
+++ source/PathInitializer.ut.cpp
@@ -38,7 +38,7 @@ TEST_CASE("PathInitializer: static operations", "[CutoutName], [CutoutFormat], [
             { PathInitializer_UnitTest("..a."), "." },
             { PathInitializer_UnitTest("..a.abcd"), "." },
             { PathInitializer_UnitTest("..abcd."), "." },
-            { PathInitializer_UnitTest("..abcd.abcd"), "." },
+            { PathInitializer_UnitTest("..abcd.abcd"), "." }
         };
 
         for (const auto& test : tests) {
@@ -52,10 +52,94 @@ TEST_CASE("PathInitializer: static operations", "[CutoutName], [CutoutFormat], [
     //_______________________________________________________________________________________________________
     //-------------------------------------------------------------------------------------------------------
     WHEN("Testing the CutoutFormat()") {
+        struct test {
+            const PathInitializer_UnitTest object_;
+            const std::string expectedValue_;
+
+            std::string log() const {
+                return 
+                    "{ \"" + object_.getTestValue()
+                    + "\" == \"" + expectedValue_
+                    + "\" }";
+            }
+        };
+        const std::initializer_list<test> tests = {
+            { PathInitializer_UnitTest("\n"), "" },
+            { PathInitializer_UnitTest(""), "" },
+            { PathInitializer_UnitTest("."), "" },
+            { PathInitializer_UnitTest(".."), "" },
+            { PathInitializer_UnitTest("./"), "" },
+            { PathInitializer_UnitTest("../"), "" },
+            
+            { PathInitializer_UnitTest("a."), "." },
+            { PathInitializer_UnitTest("a.abcd"), ".abcd" },
+            { PathInitializer_UnitTest("abcd."), "." },
+            { PathInitializer_UnitTest("abcd.abcd"), ".abcd" },
+
+            { PathInitializer_UnitTest(".a."), "." },
+            { PathInitializer_UnitTest(".a.abcd"), ".abcd" },
+            { PathInitializer_UnitTest(".abcd."), "." },
+            { PathInitializer_UnitTest(".abcd.abcd"), ".abcd" },
+    
+            { PathInitializer_UnitTest("..a."), ".a." },
+            { PathInitializer_UnitTest("..a.abcd"), ".a.abcd" },
+            { PathInitializer_UnitTest("..abcd."), ".abcd." },
+            { PathInitializer_UnitTest("..abcd.abcd"), ".abcd.abcd" }
+        };
+
+        for (const auto& test : tests) {
+            THEN(test.log()) {
+                REQUIRE_NOTHROW(test.object_.CutoutFormat());
+                auto result = test.object_.CutoutFormat();
+                REQUIRE(result == test.expectedValue_);
+            }
+        }
     }
     //_______________________________________________________________________________________________________
     //-------------------------------------------------------------------------------------------------------
     WHEN("Testing the AppendConditionalSlash()") {
+        struct test {
+            const PathInitializer_UnitTest object_;
+            const std::string expectedValue_;
+
+            std::string log() const {
+                return 
+                    "{ \"" + object_.getTestValue()
+                    + "\" == \"" + expectedValue_
+                    + "\" }";
+            }
+        };
+        const std::initializer_list<test> tests = {
+            { PathInitializer_UnitTest("\n"), "\n/" },
+            { PathInitializer_UnitTest(""), "" },
+            { PathInitializer_UnitTest("."), "./" },
+            { PathInitializer_UnitTest(".."), "../" },
+            { PathInitializer_UnitTest("./"), "./" },
+            { PathInitializer_UnitTest("../"), "../" },
+            
+            { PathInitializer_UnitTest("a."), "a./" },
+            { PathInitializer_UnitTest("a.abcd"), "a.abcd/" },
+            { PathInitializer_UnitTest(".abcd."), ".abcd./" },
+            { PathInitializer_UnitTest("..abcd.abcd"), "..abcd.abcd/" },
+
+            { PathInitializer_UnitTest("a./"), "a./" },
+            { PathInitializer_UnitTest("a.abcd/"), "a.abcd/" },
+            { PathInitializer_UnitTest(".abcd./"), ".abcd./" },
+            { PathInitializer_UnitTest("..abcd.abcd/"), "..abcd.abcd/" },
+
+            { PathInitializer_UnitTest("a.//"), "a.//" },
+            { PathInitializer_UnitTest("a.abcd//"), "a.abcd//" },
+            { PathInitializer_UnitTest(".abcd.//"), ".abcd.//" },
+            { PathInitializer_UnitTest("..abcd.abcd//"), "..abcd.abcd//" }
+        };
+
+        for (const auto& test : tests) {
+            THEN(test.log()) {
+                REQUIRE_NOTHROW(test.object_.AppendConditionalSlash());
+                auto result = test.object_.AppendConditionalSlash();
+                REQUIRE(result == test.expectedValue_);
+            }
+        }
     }
 }
 
