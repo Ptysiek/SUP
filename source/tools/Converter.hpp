38ba040297d7c0eb9e47b0bb84f23b10fa98086c
Kacu <k.ptychu@gmail.com>
2021-02-25 23:37:49 +0100

    new operation: Converter::removeWhitespaces()

--- source/tools/Converter.hpp
+++ source/tools/Converter.hpp
@@ -22,20 +22,62 @@ public:
 
     static std::string to_string(const std::vector<std::string>& data) {
         std::stringstream result;       
-        std::for_each(data.begin(), data.end(), [&](const auto& line){result << line << "\n";});
+        std::for_each(data.begin(), data.end(), [&](const auto& line){result << removeWhitespaces(line) << "\n";});
         return result.str();
     }
 
     static std::string removeWhitespaces(const std::string& data) {
-        std::stringstream result;       
-        for (const auto& character : data) {
-            if (character == ' ' || character == '\t' || character == '\n') {
+        std::string result = data;
+        result = RemoveSpecialChars(result);
+        result = RemoveFromBegin(result);
+        result = RemoveFromEnd(result);
+        result = RemoveAdjacentSpaces(result);
+        return result;
+    }
+
+private:
+    static std::string RemoveSpecialChars(const std::string& data) {
+        std::string result = data;
+        std::for_each(begin(result), end(result), 
+            [](auto& ch){ ch = (ch == '\n' || ch == '\t')? ' ' : ch; });
+        return result;
+    }
+    static std::string RemoveFromBegin(const std::string& data) {
+        size_t i = 0;
+        for ( ; i < data.size(); ++i) {
+            if (data[i] != ' ') {
+                break;
+            }
+        }
+        return data.substr(i);
+    }
+    static std::string RemoveFromEnd(const std::string& data) {
+        int i = static_cast<int>(data.size()) - 1;
+        for ( ; i >= 0; --i) {
+            if (data[i] != ' ') {
+                break;
+            }
+        }
+        return data.substr(0, i+1);
+    }
+    static std::string RemoveAdjacentSpaces(const std::string& data) {
+        std::stringstream result;
+        bool foundSpace = false;
+        for (size_t i = 0; i < data.size(); ++i) {
+            if (!foundSpace) {
+                result << data[i];
+                foundSpace = (data[i] == ' ');
                 continue;
             }
-            result << character;
+            foundSpace = (data[i] == ' ');
+            if (!foundSpace) {
+                result << data[i];
+            }
         }
         return result.str();
     }
+
+    //static std::vector<std::string> removeEmptyLines(
 
 };
 } // namespace Tools
